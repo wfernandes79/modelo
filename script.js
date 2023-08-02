e2dbd48e2011697093b3ab428f4e3d4706b82251
Willian de Souza Fernandes <109435198+wfernandes79@users.noreply.github.com>
2023-08-02 17:38:37 -0300

    madru

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas.`
     
     if (hora >= 0 && hora < 6) {
-        // Boa madrugada
+        // Boa madrugada!
         img.src = 'madrugada.png'
         document.body.style.background = `#515155`
+        
     } else if (hora >= 6 && hora < 12) {
         // Bom dia!
         img.src = 'manha.png'
